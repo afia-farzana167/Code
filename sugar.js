a162b0f1e27e7b2974e9fa497aec18dd77103f3b
afia-farzana <afar0035@student.monash.edu>
2024-10-06 01:07:40 +1000

    Now 3 charts

---
+++ sugar.js
@@ -0,0 +1,4 @@
+var vg_3 = "Sugar.json";
+vegaEmbed("#sugar", vg_3).then(function(result) {
+// Access the Vega view instance ("https://vega.github.io/vega/docs/api/view/") as result.view
+}).catch(console.error);
