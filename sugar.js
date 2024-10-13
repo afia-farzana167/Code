1cf27281faadc87d07189a590fd5906a7d91a311
afia-farzana <afar0035@student.monash.edu>
2024-10-13 18:16:03 +1100

    s

--- sugar.js
+++ sugar.js
@@ -1,4 +1,4 @@
 var vg_3 = "Sugar.json";
-vegaEmbed("#sugar", vg_3, { actions: false }).then(function(result) {
+vegaEmbed("#sugar", vg_3).then(function(result) {
 // Access the Vega view instance ("https://vega.github.io/vega/docs/api/view/") as result.view
 }).catch(console.error);
